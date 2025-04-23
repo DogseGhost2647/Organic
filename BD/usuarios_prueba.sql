041a7b40f19e8b3215b65d3c3762467c5346df8c
jose <jodadima09@gmail.com>
2025-04-23 00:28:52 -0500

    Eliminar archivos de prueba

--- BD/usuarios_prueba.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO usuarios (nombre, correo, telefono, direccion, password, rol, created_at, updated_at) VALUES
-('Ana Torres', 'ana@example.com', '3001234567', 'Calle 10 #12-34', '123456', 'cliente', NOW(), NOW()),
-('Carlos Pérez', 'carlos@example.com', '3109876543', 'Carrera 45 #76-89', 'abcdef', 'administrador', NOW(), NOW());

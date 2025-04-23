c17b52a5e7ca52f818c0d25c0449391f9e91fee3
jose <jodadima09@gmail.com>
2025-04-23 00:21:22 -0500

    Generador de reportes funcional

--- Organic/public/FPDF/Reporte.php
+++ Organic/public/FPDF/Reporte.php
@@ -10,10 +10,10 @@ class PDF extends FPDF
    // Cabecera de página
    function Header()
    {
-      //include '../../recursos/Recurso_conexion_bd.php';//llamamos a la conexion BD
+      include '../../app/Models/conexion.php';//llamamos a la conexion BD
 
-      //$consulta_info = $conexion->query(" select *from hotel ");//traemos datos de la empresa desde BD
-      //$dato_info = $consulta_info->fetch_object();
+      $consulta_info = $conexion->query(" select *from productos ");//traemos datos de la empresa desde BD
+      $dato_info = $consulta_info->fetch_object();
       $this->Image('logo.png', 5, 5, 20); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
       $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
       $this->Cell(20); // Movernos a la derecha
@@ -82,11 +82,7 @@ class PDF extends FPDF
    }
 }
 
-//include '../../recursos/Recurso_conexion_bd.php';
-//require '../../funciones/CortarCadena.php';
-/* CONSULTA INFORMACION DEL HOSPEDAJE */
-//$consulta_info = $conexion->query(" select *from hotel ");
-//$dato_info = $consulta_info->fetch_object();
+include '../../app/Models/conexion.php';//llamamos a la conexion BD
 
 $pdf = new PDF();
 $pdf->AddPage(); /* aqui entran dos para parametros (horientazion,tamaño)V->portrait H->landscape tamaño (A3.A4.A5.letter.legal) */
@@ -94,20 +90,24 @@ $pdf->AliasNbPages(); //muestra la pagina / y total de paginas
 
 $i = 0;
 $pdf->SetFont('Arial', '', 12);
-$pdf->SetDrawColor(163, 163, 163); //colorBorde
+$pdf->SetDrawColor(0, 0, 0); //colorBorde
 
-/*$consulta_reporte_alquiler = $conexion->query("  ");*/
+$consulta_reporte_productos = $conexion->query("
+   SELECT id, nombre, precio, cantidad_disponible, estado
+   FROM productos
+   ");
 
-/*while ($datos_reporte = $consulta_reporte_alquiler->fetch_object()) {      
-   }*/
-$i = $i + 1;
-/* TABLA */
-$pdf->Cell(35, 10, utf8_decode('ID'), 1, 0, 'C', 1);
-$pdf->Cell(45, 10, utf8_decode('Nombre'), 1, 0, 'C', 1);
-$pdf->Cell(35, 10, utf8_decode('Precio'), 1, 0, 'C', 1);
-$pdf->Cell(35, 10, utf8_decode('Stock'), 1, 0, 'C', 1);
-$pdf->Cell(40, 10, utf8_decode('Estado'), 1, 1, 'C', 1);
+while ($datos_reporte = $consulta_reporte_productos->fetch_object()) {      
+   $i = $i + 1;
+   /* TABLA */
+   $pdf->Cell(35, 10, utf8_decode($datos_reporte->id), 1, 0, 'C', 0);
+   $pdf->Cell(45, 10, utf8_decode($datos_reporte->nombre), 1, 0, 'C', 0);
+   $pdf->Cell(35, 10, utf8_decode($datos_reporte->precio), 1, 0, 'C', 0);
+   $pdf->Cell(35, 10, utf8_decode($datos_reporte->cantidad_disponible), 1, 0, 'C', 0);
+   $pdf->Cell(40, 10, utf8_decode($datos_reporte->estado), 1, 0, 'C', 0);
 
+   $pdf->Ln(); //salto de pagina desp de cada iteración
+   }
 
 ob_clean(); // Limpia cualquier salida previa
-$pdf->Output('Prueba.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
+$pdf->Output('Reporte productos.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
